418475461350d4d620412c12e58dad1019e5b00b
Spencer Knowlton <knowltonspencer@gmail.com>
2019-05-04 19:29:42 -0600

    dir1 added

---
+++ loops.js
@@ -0,0 +1,9 @@
+//learning loops
+// as long as condition is true loop will continue to run
+//irerator
+//for loop
+//for(setup, condition, iterator){}
+//REMEMBER ++ adds one to value
+for(var i = 0; i < 10; i++){
+    console.log(i)
+}
